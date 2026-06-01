a8b3543f71ff6b815e0e2deb631ff4c54d7faae8
Nikhilesh Kumar <nikhileshkumar2509@gmail.com>
2026-06-01 18:25:28 +0530

    conditional rendering of form on the basis of useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,31 @@ import { useState } from "react";
 function App() {
   let {register, handleSubmit, reset} = useForm();
   let [bmi,  setBmi] = useState(0);
+  let [isFormVisible, setIsFormVisible] = useState(true);
   
 
   function bmiCalculator(data){
     let height =  data.height;
     let weight =  data.weight;
     setBmi(Math.floor(weight/(height*height)));
+    setIsFormVisible(!isFormVisible);
     reset();
   }
 
   return (
     <>
       {
-        bmi === 0 ?
+        isFormVisible ? 
         <form action="" onSubmit={handleSubmit(bmiCalculator)}>
-          <div className='w-80 border border-neutral-400 mx-auto mt-40 px-4 py-2 rounded-lg shadow-lg'>
-              <h1 className='text-center text-lg text-neutral-600 font-semibold'>Check Your Body Mass Index</h1>
-              <input {...register('height')} className='w-full text-sm text-neutral-600 font-medium my-2 border border-neutral-300 rounded-lg py-1 px-1 outline-none' type="text" placeholder='Enter your height in metres' />
-              <input {...register('weight')} className='w-full text-sm text-neutral-600 font-medium my-2 border border-neutral-300 rounded-lg py-1 px-1 outline-none' type="text" placeholder='Enter your weight in Kgs' />
-              <input className='w-full bg-black text-white px-2 py-1 rounded-lg mt-2 inline mb-1' type="submit" value='GET BMI' />
-          </div>
-        </form>
-        :
-        <Card bmi={bmi} />
+        <div className='w-80 border border-neutral-400 mx-auto mt-40 px-4 py-2 rounded-lg shadow-lg'>
+            <h1 className='text-center text-lg text-neutral-600 font-semibold'>Check Your Body Mass Index</h1>
+            <input {...register('height')} className='w-full text-sm text-neutral-600 font-medium my-2 border border-neutral-300 rounded-lg py-1 px-1 outline-none' type="text" placeholder='Enter your height in metres' />
+            <input {...register('weight')} className='w-full text-sm text-neutral-600 font-medium my-2 border border-neutral-300 rounded-lg py-1 px-1 outline-none' type="text" placeholder='Enter your weight in Kgs' />
+            <input className='w-full bg-black text-white px-2 py-1 rounded-lg mt-2 inline mb-1' type="submit" value='GET BMI' />
+        </div>
+      </form>
+      :
+      <Card bmi={bmi} />
       }
       
     </>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card({bmi}) {
   return (
     <>
-        <div className="w-[200px] border mx-auto mt-36 p-2 border-neutral-400 shadow-xl rounded-lg">
+        <div className="w-[200px] border mx-auto mt-36 p-2 border-neutral-400 shadow-xl rounded-lg items-center">
             <h1 className='text-center text-xl text-neutral-600 font-semibold'>Your BMI is : {bmi}</h1>
         </div>
     </>
